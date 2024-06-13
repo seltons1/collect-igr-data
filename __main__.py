6262f5b6a23ceaeff3fbb926d2c45b5f8f625330
elton.santos <seltons1@gmail.com>
2024-06-13 15:53:19 -0300

    Revert "Downloading and reading file .csv"
    This reverts commit 70eb2ca02d95323e80da4d61d094e4c556050de0.

--- __main__.py
+++ __main__.py
@@ -1,29 +1,15 @@
 import os
+import datetime
 import wget
-import pandas as pd
 
 # Defining aux variables
 
 FILE_NAME = "IGR.csv"
 
-YEAR = "2023"
-
-URL = f"""https://dadosabertos.ans.gov.br/FTP/PDA/IGR/IGR_versao_{YEAR}/{FILE_NAME}"""
+URL = f"""https://dadosabertos.ans.gov.br/FTP/PDA/IGR/IGR_versao_2023/{FILE_NAME}"""
 
 PARQUET_NAME = "IGR.parquet"
 
-def download_file():
-
-    wget.download(URL, FILE_NAME)
-
-    return True
-
-def read_file():
-
-    df = pd.read_csv(FILE_NAME, sep=';', on_bad_lines='skip')
-    return df
 
 if __name__ == "__main__":
-    
-    download_file()
-    print(read_file().head())
+    pass
